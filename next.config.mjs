a88b9f0c5a3a6ed418bbf27ce829d72657658c1d
dhr232 <dhruvilupadhyay@gmail.com>
2026-09-22 01:01:36 -0700

    reconfig libraries

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
-import type { NextConfig } from "next";
 import path from "path";
 import { fileURLToPath } from "url";
 
 const root = path.dirname(fileURLToPath(import.meta.url));
 
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   turbopack: {
     root,
   },
